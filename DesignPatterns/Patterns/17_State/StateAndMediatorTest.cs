603be5a4b77ef62b309ca4cea4dd8f4e3d2c401d
MineFurjiko <877008992@qq.com>
2016-11-10 10:34:25 +0800

    New

---
+++ DesignPatterns/Patterns/17_State/StateAndMediatorTest.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatterns.Patterns._Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns._17_State {
+	class StateAndMediatorTest:IMain {
+		public void AppMain() {
+			throw new NotImplementedException();
+		}
+	}
+}
